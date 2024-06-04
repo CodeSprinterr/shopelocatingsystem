5994f6772a37f17e40a1fde0bbefa28dbf9d7056
Aakash Vaidya <aakashvaidya007@gmail.com>
2024-06-04 21:21:08 +0530

    first

--- shoplocatingsystem/urls.py
+++ shoplocatingsystem/urls.py
@@ -23,6 +23,7 @@ from django.urls.conf import include
 urlpatterns = [
     path('',include('shoplocatingapp.urls')),
     path('admin/', admin.site.urls),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
